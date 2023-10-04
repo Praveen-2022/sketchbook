7ebb68041149fdfb135b40c3313362cb9173b21e
Praveen-2022 <praveenkumarchauhan2022@gmail.com>
2023-10-04 08:36:36 +0530

    board ui completed

--- client/src/components/Board/index.js
+++ client/src/components/Board/index.js
@@ -1,6 +1,19 @@
-import React from 'react'
+import { useRef,useEffect } from "react"
+
 
 export default function Board() {
+  const canvasRef =useRef(null);
+
+  useEffect(()=>{
+    if(!canvasRef.current) return;
+    const canvas =canvasRef.current;
+    const context=canvas.getContext('2d');
+
+    // while mounting canvas it take display width
+    canvas.width =window.innerWidth;
+    canvas.height =window.innerHeight;
+  },[]);
+
   return (
     <div>Board</div>
   )
