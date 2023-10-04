c5712398a34542652056fbd1dc13d166ec2ce19b
Praveen-2022 <praveenkumarchauhan2022@gmail.com>
2023-10-04 10:30:59 +0530

    final commit

---
+++ client/src/slice/menuSlice.js
@@ -0,0 +1,27 @@
+// slices => whatever done in menu components are store in menu Slice
+import { createSlice } from "@reduxjs/toolkit";
+import { MENU_ITEMS } from "@/co nstants";
+
+const initialState = {
+  // Pencil and Eraser are active items
+  activeMenuItem: MENU_ITEMS.PENCIL,
+  // all others are actionable
+  actionMenuItem: null,
+};
+
+export const menuSlice = createSlice({
+  name: "menu",
+  initialState,
+  reducers: {
+    menuItemClick: (state, action) => {
+      state.activeMenuItem = action.payload;
+    },
+    actionItemClick: (state, action) => {
+      state.actionMenuItem = action.payload;
+    },
+  },
+});
+
+export const { menuItemClick, actionItemClick } = menuSlice.actions;
+
+export default menuSlice.reducer;
