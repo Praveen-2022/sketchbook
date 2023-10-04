78bf86704a7bdcba81d0fed67aaf24511286e0f7
Praveen-2022 <praveenkumarchauhan2022@gmail.com>
2023-10-04 21:44:45 +0530

    update : Board

--- client/src/components/Board/index.js
+++ client/src/components/Board/index.js
@@ -1,20 +1,26 @@
-import { useRef,useEffect } from "react"
+import { useEffect, useLayoutEffect, useRef } from "react";
+import { useSelector, useDispatch } from "react-redux";
 
+import { MENU_ITEMS } from "@/constants";
+import { actionItemClick } from "@/slice/menuSlice";
 
 export default function Board() {
-  const canvasRef =useRef(null);
+  const canvasRef = useRef(null);
+  const activeMenuItem = useSelector((state) => state.menu.activeMenuItem);
+  const {color,size} =useSelector((state)=>state.toolbox[activeMenuItem]);
+ 
 
-  useEffect(()=>{
-    if(!canvasRef.current) return;
-    const canvas =canvasRef.current;
-    const context=canvas.getContext('2d');
+
+useEffect(() => {
+    if (!canvasRef.current) return;
+    const canvas = canvasRef.current;
+    const context = canvas.getContext("2d");
 
     // while mounting canvas it take display width
-    canvas.width =window.innerWidth;
-    canvas.height =window.innerHeight;
-  },[]);
-
-  return (
-    <div>Board</div>
-  )
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+  }, []);
+  // it print select value and brush size
+// console.log(color,size)
+  return (<canvas ref={canvasRef}></canvas>);
 }
