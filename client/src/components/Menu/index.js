efc077fa30e4e8bd8372d9fcf3c7cb29990503d5
Praveen-2022 <praveenkumarchauhan2022@gmail.com>
2023-10-05 20:43:34 +0530

    code updatation due to ui error

--- client/src/components/Menu/index.js
+++ client/src/components/Menu/index.js
@@ -17,6 +17,7 @@ import { MENU_ITEMS } from "@/constants";
 const Menu = () => {
   const dispatch = useDispatch();
   const activeMenuItem = useSelector((state) => state.menu.activeMenuItem);
+ 
   const handleMenuClick = (itemName) => {
     dispatch(menuItemClick(itemName));
   };
@@ -53,68 +54,3 @@ const Menu = () => {
 
 export default Menu;
 
-// import styles from "./index.module.css";
-// import cx from "classnames";
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import {
-//   faPencil,
-//   faEraser,
-//   faRotateLeft,
-//   faRotateRight,
-//   faFileArrowDown,
-// } from "@fortawesome/free-solid-svg-icons";
-
-// import { menuItemClick, actionItemClick } from "@/slice/menuSlice";
-// import { useDispatch, useSelector } from "react-redux";
-
-// import { MENU_ITEMS } from "@/constants";
-// export default function Menu() {
-//   const dispatch = useDispatch();
-//   const activeMenuItem = useSelector((state) => state.menu.activeMenuItem);
-//   const handleMenuClick = (itemName) => {
-//     dispatch(menuItemClick(itemName));
-//   };
-
-//   const handleActioItemClick = (itemName) => {
-//     dispatch(actionItemClick(itemName));
-//   };
-//   return (
-//     <div className={styles.menuContainer}>
-//       <div
-//         // cx is use for apply dynamic css classes , avoid using "",? && conditional statement
-//         className={cx(styles.iconWrapper, {
-//           [styles.active]: activeMenuItem === MENU_ITEMS.PENCIL,
-//         })}
-//         onClick={() => handleMenuClick(MENU_ITEMS.PENCIL)}
-//       >
-//         <FontAwesomeIcon icon={faPencil} className={styles.icon} />
-//       </div>
-//       <div
-//         className={cx(styles.iconWrapper, {
-//           [styles.active]: activeMenuItem === MENU_ITEMS.ERASER,
-//         })}
-//         onClick={() => handleMenuClick(MENU_ITEMS.ERASER)}
-//       >
-//         <FontAwesomeIcon icon={faEraser} className={styles.icon} />
-//       </div>
-//       <div
-//         className={styles.iconWrapper}
-//         onClick={() => handleActioItemClick(MENU_ITEMS.UNDO)}
-//       >
-//         <FontAwesomeIcon icon={faRotateLeft} className={styles.icon} />
-//       </div>
-//       <div
-//         className={styles.iconWrapper}
-//         onClick={() => handleActioItemClick(MENU_ITEMS.REDO)}
-//       >
-//         <FontAwesomeIcon icon={faRotateRight} className={styles.icon} />
-//       </div>
-//       <div
-//         className={styles.iconWrapper}
-//         onClick={() => handleActioItemClick(MENU_ITEMS.DOWNLOAD)}
-//       >
-//         <FontAwesomeIcon icon={faFileArrowDown} className={styles.icon} />
-//       </div>
-//     </div>
-//   );
-// }
